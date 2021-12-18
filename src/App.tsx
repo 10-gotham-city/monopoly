fcb88be7217ed8012085297a138e6add9a44cb0a
KovAndrey <58121894+AndreyKovalenok@users.noreply.github.com>
2021-12-18 18:39:17 +0200

    chore: init project (#1)
    * chore: init project
    
    * chore: add gitignore
    
    * chore: add webpack
    
    * chore: add react & ts & scripts
    
    * chore: add tmp tsconfig
    
    * chore: add webpack config
    
    * feat(app): add entry app file
    
    * chore: add airbnb config
    
    * chore: add nvmrc
    
    * chore: add husky & pre-commit hook
    
    * chore: add eslintignore
    
    * chore: add formik
    
    * chore: add mui
    
    * chore: add noImplicitAny rule for ts
    
    * refactor(app): use named exports
    
    Co-authored-by: kv <kv@kode.ru>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { Typography } from '@mui/material';
+
+export const App = () => (
+  <>
+    <CssBaseline />
+    <Typography variant="h1">App</Typography>
+  </>
+);
