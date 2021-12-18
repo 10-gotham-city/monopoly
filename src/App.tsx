aa8d9042b6d647e0073cd31976104e54dc502df2
kv <kv@kode.ru>
2021-12-18 09:59:20 +0200

    refactor(app): use named exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { Typography } from '@mui/material';
 
-const App = () => (
+export const App = () => (
   <>
     <CssBaseline />
     <Typography variant="h1">App</Typography>
   </>
 );
-
-export default App;
