464db82927e147105210e05b69c8aee16b29f95a
Dmitry V <volodin.dmitry92@gmail.com>
2021-12-20 23:12:58 +0300

    rename directory shared/api

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from 'shared/API/config';
+import { BASE_URL } from 'shared/api/config';
 
 export const instanceAxios = axios.create({
   baseURL: BASE_URL,
