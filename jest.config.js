9a906ba8e582463fbce73b891d30002435a60d18
Dmitry V <volodin.dmitry92@gmail.com>
2022-01-29 19:48:32 +0300

    fix(jest): delete excess package

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  verbose: true,
 };
