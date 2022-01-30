9fe21101b9394f3f3af8b061e084c3904371d6b2
kv <kv@kode.ru>
2022-01-30 14:05:21 +0200

    fix(input-field): remove typography wrap for error message

--- src/shared/ui/components/input-field/input-field.tsx
+++ src/shared/ui/components/input-field/input-field.tsx
@@ -1,4 +1,4 @@
-import { TextField, Typography, useTheme } from '@mui/material';
+import { TextField } from '@mui/material';
 import { Field, FieldProps } from 'formik';
 import { ComponentProps, InputHTMLAttributes, memo } from 'react';
 
@@ -10,30 +10,24 @@ type Props = {
   inputProps?: ComponentProps<typeof TextField>['InputProps'];
 };
 
-export const InputField = memo(({ name, label, type = 'text', inputProps, required }: Props) => {
-  const theme = useTheme();
-
-  return (
-    <Field name={name}>
-      {({ field, meta, form: { isSubmitting } }: FieldProps<unknown>) => (
-        <TextField
-          name={name}
-          id={name}
-          type={type}
-          label={label}
-          value={field.value}
-          error={meta.touched && Boolean(meta.error)}
-          disabled={isSubmitting}
-          required={required}
-          helperText={
-            meta.touched && <Typography color={theme.palette.error.main}>{meta.error}</Typography>
-          }
-          onChange={field.onChange}
-          onBlur={field.onBlur}
-          fullWidth
-          InputProps={inputProps}
-        />
-      )}
-    </Field>
-  );
-});
+export const InputField = memo(({ name, label, type = 'text', inputProps, required }: Props) => (
+  <Field name={name}>
+    {({ field, meta, form: { isSubmitting } }: FieldProps<unknown>) => (
+      <TextField
+        name={name}
+        id={name}
+        type={type}
+        label={label}
+        value={field.value}
+        error={meta.touched && Boolean(meta.error)}
+        disabled={isSubmitting}
+        required={required}
+        helperText={meta.touched && meta.error}
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+        fullWidth
+        InputProps={inputProps}
+      />
+    )}
+  </Field>
+));
