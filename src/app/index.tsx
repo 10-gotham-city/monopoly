9a14b3bdcc836b75a7efd482349ab5c1606ac179
anikinaa <51367127+anikinaa@users.noreply.github.com>
2022-01-30 18:35:28 +0300

    53/stylization (#32)
    * dark theme
    
    * add user profile
    
    * fix conversation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,28 @@
+import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Router } from 'pages';
 
+import { theme } from 'shared/ui/theme';
+
 import { AuthProvider, ErrorBoundary, SnackbarProvider } from './providers';
 import { reduxStore } from './store';
 
 export const App = () => (
-  <ErrorBoundary>
-    <CssBaseline />
-    <SnackbarProvider>
-      <ReduxProvider store={reduxStore}>
-        <BrowserRouter>
-          <AuthProvider>
-            <Router />
-          </AuthProvider>
-        </BrowserRouter>
-      </ReduxProvider>
-    </SnackbarProvider>
-  </ErrorBoundary>
+  <ThemeProvider theme={theme}>
+    <ErrorBoundary>
+      <CssBaseline />
+      <SnackbarProvider>
+        <ReduxProvider store={reduxStore}>
+          <BrowserRouter>
+            <AuthProvider>
+              <Router />
+            </AuthProvider>
+          </BrowserRouter>
+        </ReduxProvider>
+      </SnackbarProvider>
+    </ErrorBoundary>
+  </ThemeProvider>
 );
