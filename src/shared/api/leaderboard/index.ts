625be8e008a9b94aff9b44fee872f6d3b742de66
Dmitry <51696445+DmV46@users.noreply.github.com>
2022-02-07 22:18:19 +0300

    feat(api): add api for leaderboard (#29)
    * feat(api): add api for leaderboard
    
    * feat(api): add method for teamLeaderboard

---
+++ src/shared/api/leaderboard/index.ts
@@ -0,0 +1,13 @@
+export {
+  leaderboardApi,
+  useAddUserToLeaderboardMutation,
+  useGetTeamLeaderboardQuery,
+  useGetAllLeaderboardQuery,
+} from './leaderboard-api';
+
+export {
+  TUserLeaderboardRequest,
+  TAllLeaderboardRequest,
+  TAllLeaderboardResponse,
+  TTeamLeaderboardRequest,
+} from './types';
