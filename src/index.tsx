83700dc62f80ed51fe7f5d18b0571b0972b3d800
anikinaa <51367127+anikinaa@users.noreply.github.com>
2022-03-07 20:08:59 +0300

    35/sw (#39)
    * feat add serves worker
    
    * fix(sw): verion Dependencies
    
    Co-authored-by: kv <kv@kode.ru>

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { hydrate } from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -21,3 +25,16 @@ hydrate(
   </ReduxProvider>,
   document.getElementById('root'),
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
